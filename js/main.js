49b8e82d43145ff3e09d7dfa3280cd5da17b1ab0
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-09 15:53:53 +0100

    team card output in DOM

--- js/main.js
+++ js/main.js
@@ -2,37 +2,51 @@ const team = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'wayne-barnett-founder-ceo.jpg',
+        image: 'img/wayne-barnett-founder-ceo.jpg',
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'angela-caroll-chief-editor.jpg',
+        image: 'img/angela-caroll-chief-editor.jpg',
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'walter-gordon-office-manager.jpg',
+        image: 'img/walter-gordon-office-manager.jpg',
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: 'angela-lopez-social-media-manager.jpg',
+        image: 'img/angela-lopez-social-media-manager.jpg',
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        image: 'scott-estrada-developer.jpg',
+        image: 'img/scott-estrada-developer.jpg',
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        image: 'barbara-ramos-graphic-designer.jpg',
+        image: 'img/barbara-ramos-graphic-designer.jpg',
     },
 ];
 
 
 for(let i = 0 ; i < team.length ; i++){
-    // console.log("Membro " + [i + 1] + " " + team[i]);
     console.log(team[i]);
 }
+
+let teamCard = '';
+
+for (let i = 0 ; i < team.length ; i++){
+    teamCard += `
+    <div class="my-card">
+        <img class="pb-2" src="${team[i].image}" alt="Team member picture">
+        <p> ${team[i].name} </p>
+        <p> ${team[i].role} </p>
+    </div>
+    `
+}
+
+const cardWrapper = document.querySelector('#pictures');
+cardWrapper.innerHTML += teamCard;
