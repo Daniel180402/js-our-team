ee980564ef49caab66d3eade03454c5cc5442ac3
Daniel Jarnig <danieljarnig1804@gmail.com>
2022-03-09 16:32:48 +0100

    fixed minor problem with image

--- js/main.js
+++ js/main.js
@@ -2,32 +2,32 @@ const team = [
     {
         name: 'Wayne Barnett',
         role: 'Founder & CEO',
-        image: 'img/wayne-barnett-founder-ceo.jpg',
+        image: 'wayne-barnett-founder-ceo.jpg',
     },
     {
         name: 'Angela Caroll',
         role: 'Chief Editor',
-        image: 'img/angela-caroll-chief-editor.jpg',
+        image: 'angela-caroll-chief-editor.jpg',
     },
     {
         name: 'Walter Gordon',
         role: 'Office Manager',
-        image: 'img/walter-gordon-office-manager.jpg',
+        image: 'walter-gordon-office-manager.jpg',
     },
     {
         name: 'Angela Lopez',
         role: 'Social Media Manager',
-        image: 'img/angela-lopez-social-media-manager.jpg',
+        image: 'angela-lopez-social-media-manager.jpg',
     },
     {
         name: 'Scott Estrada',
         role: 'Developer',
-        image: 'img/scott-estrada-developer.jpg',
+        image: 'scott-estrada-developer.jpg',
     },
     {
         name: 'Barbara Ramos',
         role: 'Graphic Designer',
-        image: 'img/barbara-ramos-graphic-designer.jpg',
+        image: 'barbara-ramos-graphic-designer.jpg',
     },
 ];
 
@@ -41,7 +41,7 @@ let teamCard = '';
 for (let i = 0 ; i < team.length ; i++){
     teamCard += `
     <div class="my-card">
-        <img class="pb-2" src="${team[i].image}" alt="Team member picture">
+        <img class="pb-2" src="${"img/" + team[i].image}" alt="Team member picture">
         <p> ${team[i].name} </p>
         <p> ${team[i].role} </p>
     </div>
